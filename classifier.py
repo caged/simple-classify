be1fbf30c44c7d014f3ca58f6e5963af7f66aafe
Justin Palmer <justin@labratrevenge.com>
2016-10-01 14:03:27 -0700

    genesis

---
+++ classifier.py
@@ -0,0 +1,41 @@
+import numpy as np
+import tflearn
+import pprint
+
+# Download the Titanic dataset
+from tflearn.datasets import titanic
+titanic.download_dataset('titanic_dataset.csv')
+
+# Load CSV file, indicate that the first column represents labels
+from tflearn.data_utils import load_csv
+data, labels = load_csv('titanic_dataset.csv', target_column=0,
+                        categorical_labels=True, n_classes=2)
+
+# Preprocessing function
+def preprocess(data, columns_to_ignore):
+    # Sort by descending id and delete columns
+    for id in sorted(columns_to_ignore, reverse=True):
+        [r.pop(id) for r in data]
+    for i in range(len(data)):
+      # Converting 'sex' field to float (id is 1 after removing labels column)
+      data[i][1] = 1. if data[i][1] == 'female' else 0.
+    return np.array(data, dtype=np.float32)
+
+# Ignore 'name' and 'ticket' columns (id 1 & 6 of data array)
+to_ignore=[1, 6]
+
+# Preprocess data
+data = preprocess(data, to_ignore)
+
+
+# Build neural network
+net = tflearn.input_data(shape=[None, 6])
+net = tflearn.fully_connected(net, 32)
+net = tflearn.fully_connected(net, 32)
+net = tflearn.fully_connected(net, 2, activation='softmax')
+net = tflearn.regression(net)
+
+# Define model
+model = tflearn.DNN(net)
+# Start training (apply gradient descent algorithm)
+model.fit(data, labels, n_epoch=10, batch_size=16, show_metric=True)
